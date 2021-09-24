0735eec2675caa38fbd457000466e41c47575157
mdriyas-ui <mdriyazath19@gmail.com>
2021-09-24 10:27:52 +0530

    view image function

--- api-routes/employee.routes.js
+++ api-routes/employee.routes.js
@@ -7,42 +7,38 @@ const Image = mongoose.model('Image');
 
 
 const storage = multer.diskStorage({
-    destination:(req,res,callback)=>{
-        callback(null,'./uploads/')
+    destination: (req, res, callback) => {
+        callback(null, './uploads/')
     },
-    filename :(req,file, callback)=>{
-        callback(null, new Date().toISOString()+ file.originalname)
+    filename: (req, file, callback) => {
+        callback(null, new Date().toISOString() + file.originalname)
     }
+});
+const upload = multer({
+    storage: storage
 });
 
 
-const upload = multer({storage:storage});
-
 //rest api's
-employeeRouter.get('/',(req,res)=>{
+employeeRouter.get('/', (req, res) => {
     res.send("WELCOME TO EMPLOYMENT PORTAL")
 });
 
-employeeRouter.post('/create',Employee.create);
-
-employeeRouter.get('/read',Employee.read);
-
-employeeRouter.put('/update/:_id',Employee.update);
-
-employeeRouter.delete('/delete/:_id',Employee.delete);
-
-employeeRouter.post('/upload-image',upload.single('empImage'),async(req, res)=>{
-      let imageFile = req.file;
+employeeRouter.post('/upload-image', upload.single('empImage'), async (req, res) => {
+    let imageFile = req.file;
     const imageData = new Image({
-        imageName:imageFile.filename,
-        imageUrl:imageFile.path
+        imageName: imageFile.filename,
+        imageUrl: imageFile.path
     });
-    let savedImgData = await imageData.save() 
+    let savedImgData = await imageData.save()
     console.log(savedImgData);
-    res.send(req.file);    
+    res.send(req.file);
 });
 
-employeeRouter.delete('/delete-image/:_id',Employee.deleteImage);
+
+employeeRouter.get('/view-image/:_id', Employee.viewImage);
 
 
-module.exports=employeeRouter;
+employeeRouter.delete('/delete-image/:_id', Employee.deleteImage);
+
+module.exports = employeeRouter;
--- controller/employee.business_logic.js
+++ controller/employee.business_logic.js
@@ -3,35 +3,8 @@ const Employee = mongoose.model('Employee');
 const schemaValidation = require('../helper/validate_schema');
 const fs = require('fs');
 const Image = mongoose.model('Image');
-
-
-
-exports.create = async (req, res, next) => {
-    try {
-        let employee = {
-            empName: req.body.empName,
-            empEmail: req.body.empEmail,
-            designation: req.body.designation,
-            address: req.body.address,
-        }
-        let validationResult = await schemaValidation.empSchema.validateAsync(employee)
-        let empData = new Employee(validationResult);
-        let savedEmpData = await empData.save()
-        console.log(savedEmpData);
-        res.status(200).send(`the given employees details is successfully created : ${savedEmpData}`);
-
-    } catch (error) {
-        if (error.isjoi === true) error.status = 422
-        next(error);
-    }
-};
-
-
-
-exports.read = async (req, res) => {
-    let empData = await Employee.find()
-    res.status(200).send(`employee data: ${empData}`);
-};
+require('dotenv').config();
+let path = require('path')
 
 
 
@@ -73,42 +46,42 @@ exports.update = async (req, res, next) => {
 }
 
 
-exports.delete = async (req, res) => {
-    let deletedEmpData = await Employee.deleteOne({
+
+
+exports.viewImage = async (req, res) => {
+
+    let fetchImage = await Image.find({
         _id: req.params._id
     });
+    let imageUrl = fetchImage[0].imageUrl;
+    let imagePath = path.join(__dirname, `${'../' + imageUrl}`);
+    console.log(imagePath);
+    res.sendFile(imagePath)
 
-    if (deletedEmpData.deletedCount === 0) {
-        res.status(404).send(`already deleted (or) not found in record`);
-        console.log("already deleted or not found in record");
-    } else {
-        res.status(200).send(`employee data deleted successfully`);
-    }
-
-};
+}
 
 
 
-exports.deleteImage =async (req, res)=>{
+exports.deleteImage = async (req, res) => {
 
-    let fetchImage = await Image.find({_id: req.params._id});
-    // console.log(fetchImage[0].imageUrl);   
-
-    const path = fetchImage[0].imageUrl
+    let fetchImage = await Image.find({
+        _id: req.params._id
+    });
+    let imagePath = fetchImage[0].imageUrl
     try {
-      fs.unlinkSync(path)
-      //file removed 
-      let deletedImgData = await Image.deleteOne({
-        _id: req.params._id
-    });
-    if (deletedImgData.deletedCount === 0) {
-        res.status(404).send(`already deleted (or) not found in record`);
-        console.log("already deleted or not found in record");
-    } else {
-        res.status(200).send(`employee image deleted successfully`);
-    }
+        fs.unlinkSync(imagePath)    // file removed
+        let deletedImgData = await Image.deleteOne({
+            _id: req.params._id
+        });
+        if (deletedImgData.deletedCount === 0) {
+            res.status(404).send(`already deleted (or) not found in record`);
+            console.log("already deleted or not found in record");
+        } else {
+            console.log("image deleted successFully");
+            res.status(200).send(`employee image deleted successfully`);            
+        }
 
-    } catch(err) {
-      console.error(err)
+    } catch (err) {
+        console.error(err)
     }
 }
